70ad11182baccf1d67ae60b5c0189f123fd29236
Oddly <mairekkgriffiths@gmail.com>
2025-12-29 05:03:11 -0700

    Removed: DataBase and moved to todo Added: Saveing of tasks

--- Todo_List.py
+++ Todo_List.py
@@ -2,8 +2,22 @@ import json
 
 class Todo:
     def __init__(self):
-        self.tasks = []
-    
+        self.tasks =self.load_todo_file()
+
+    ### Load/make json file
+    def load_todo_file(self, path = "Todo_File.json"):
+        try:
+            with open(path, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return []
+        
+    def save_todo_file(self,path = "Todo_File.json"):
+        with open(path, "w") as f:
+            json.dump(self.tasks, f, indent=2)
+        
+
+    ### Main loop    
     def run(self):
         while True:
             choice = input(
@@ -23,22 +37,13 @@ class Todo:
                 self.show_tasks()
             else:
                 print("Please choose a, s, or q.")
-class DataBase:
-    def __init__(self, path = "Todo_File.json"):
-        self.task_file = path
-
-    def load_todo_file(self):
-        try:
-            with open(self.task_file, "r") as f:
-                return json.load(f)
-        except FileNotFoundError:
-            return []
 
     def add_task(self):
         while True:
             add_new_task = input("What would you like to add?: ").strip()
             if add_new_task:
                 self.tasks.append(add_new_task)
+                self.save_todo_file()
                 print("Task added!")
             another = input("Add another Task (y/n)?: ").strip().lower()
             if another == "n":
@@ -56,6 +61,8 @@ class DataBase:
         print("Task list:")
         for i, t in enumerate(self.tasks, start=1):
             print(f"{i}. {t}")
+                
+
 
 
 if __name__ == "__main__":
